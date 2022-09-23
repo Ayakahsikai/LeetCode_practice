2cd16b326eef3ed7dc13b3cce0ef096c52161abb
Ayakahsikai <ayakashikaidevelopers@gmail.com>
2022-09-23 14:42:38 +0800

    Create 647. Palindromic Substrings.js

---
+++ Medium/647. Palindromic Substrings.js
@@ -0,0 +1,56 @@
+/**
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const s = "abc";
+
+var countSubstrings = function (s) {
+    res = 0;
+    const c = (i, j) => {
+        while(i >= 0 && j < s.length && s[i] === s[j]) {
+            res++;
+            i--;
+            j++;
+        }
+    }
+
+    for(i = 0; i < s.length; i++) {
+        c(i, i); //自己單個+1
+        c(i, i + 1); //比對字串是否是回文
+    }
+    return res;
+};
+
+countSubstrings(s);
+
+/*
+
+*/
